bd11f231d2ecd6148692bb3012af05f83f92005e
azumaya <azumaya@evocomp.cse.kyutech.ac.jp>
2015-09-09 18:30:12 +0900

    モデルの追加

---
+++ app/Model/Plan.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Plan Model
+ *
+ */
+class Plan extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+}
