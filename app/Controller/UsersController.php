69490109494a9b9416da85dea89e7c794f44fc0c
yamadaJr <hirobiro77@gmail.com>
2015-09-11 14:44:35 +0900

    1

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -70,7 +70,7 @@ public function logout() {
 			$this->User->create();
 			if ($this->User->save($this->request->data)) {
 				$this->Session->setFlash(__('登録が完了しました。ログインしてください!'));
-				return $this->redirect(array('controller' => 'plans', 'action' => 'login'));
+				return $this->redirect(array('controller' => 'users', 'action' => 'login'));
 			} else {
 				$this->Session->setFlash(__('The user could not be saved. Please, try again.'));
 			}
